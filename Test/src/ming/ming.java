4dab90186af33ce86716dfd647e84c69198fa2f6
ming <mchenchn@aliyun.com>
2018-12-18 17:24:05 +0800

    HHHH

--- Test/src/ming/ming.java
+++ Test/src/ming/ming.java
@@ -5,5 +5,8 @@ public class ming {
 	public static void main(String[] args) {
 		System.out.println("Hello,World");
 		System.out.println("BreezeCHN");
+		
+		
+		System.out.println("newnewnewnew");
 	}
 }
